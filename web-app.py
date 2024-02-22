ac47632ebac4e2d13f3aeb7e832db953c1ec6024
git-fahad <javaidfahad7@gmail.com>
2024-02-22 10:22:19 +0530

    My first Web App

---
+++ web-app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return f"Hello, {name}! Your email is {email}."
+
+if __name__ == '__main__':
+    app.run(debug=True)
